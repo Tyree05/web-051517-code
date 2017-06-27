b957b5f4ed9b00b1eda20c832678a5fdb114f3ae
Joe Burgess <joe@flatironschool.com>
2017-06-27 09:34:13 -0400

    adding Mod 3 Week 1 Day 1

---
+++ Module-3/day-1/javascript-arithmetic-lab/index.js
@@ -0,0 +1,44 @@
+/*if (true === false) {
+  console.log("truth")
+} else {
+  console.log("not truth")
+}
+*/
+/*let numbers = [1,2,3,4]
+let sum = 0
+for (let i=0;i<numbers.length;i++) {
+  sum += numbers[i]
+}
+
+let person = {"name": "Joe",
+              "height": 74}
+function sayHello(message) {
+  console.log(`The message is ${message}`)
+  //return message
+}
+
+
+sayHello("Roosevelt")
+console.log(sum)
+
+*/
+
+function add(left, right) {
+  return left + right
+}
+
+function subtract(left, right) {
+  return left - right
+}
+
+function multiply(left, right) {
+  return left * right
+}
+
+function inc(number) {
+  virtualConsole.log("#############")
+  console.log("asdf")
+  return number+1
+}
+
+console.log(inc(52))
